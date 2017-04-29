6b8dc8434ea7b0202181b2c71a8eb79ef8588dc2
Herve <h.hochart29@gmail.com>
2017-04-29 17:45:20 +0200

    Double bundle Accueil + plateforme

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
             new OC\PlatformBundle\OCPlatformBundle(),
+            new OC\AccueilBundle\OCAccueilBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
