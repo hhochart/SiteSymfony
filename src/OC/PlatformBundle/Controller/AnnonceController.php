64459bf7c5199d070959702ca7c21c02ae84f76f
Herve <h.hochart29@gmail.com>
2017-07-03 00:31:08 +0200

    Doctrine OneToOne + ManyToOne

--- src/OC/PlatformBundle/Controller/AnnonceController.php
+++ src/OC/PlatformBundle/Controller/AnnonceController.php
@@ -3,6 +3,7 @@
 namespace OC\PlatformBundle\Controller;
 
 use OC\PlatformBundle\Entity\Annonce;
+use OC\PlatformBundle\Entity\Application;
 use OC\PlatformBundle\Entity\Image;
 use OC\PlatformBundle\OCPlatformBundle;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -55,15 +56,20 @@ class AnnonceController extends Controller
         $annonce = $repertoireAnnonce->find($id_annonce);
 
         if ($annonce === null) {
-            throw new NotFoundHttpException("L'annonce avec l'ID :" . $id_annonce . " n'existe pas");
+            throw new NotFoundHttpException("L'annonce avec l'ID :".$id_annonce." n'existe pas");
         }
+
+        $listecandidatures = $em
+            ->getRepository('OCPlatformBundle:Application')
+            ->findBy(array('annonce' => $id_annonce));
 
         return $this->render(
             'OCPlatformBundle:Annonce:templateAnnonce.html.twig',
             array(
-                'annonce'         => $annonce,
-                'id_annonce_prev' => $id_annonce_prev,
-                'id_annonce_next' => $id_annonce_next,
+                'listecandidatures' => $listecandidatures,
+                'annonce'           => $annonce,
+                'id_annonce_prev'   => $id_annonce_prev,
+                'id_annonce_next'   => $id_annonce_next,
             )
         );
     }
@@ -76,6 +82,26 @@ class AnnonceController extends Controller
         $annonce->setAuteur('Auteur de la base de données');
         $annonce->setContenu('Description de la première annonce ajoutée en base de donnée !');
 
+        //création de l'entité annonce 1
+        $application1 = new Application();
+        $application1->setAuteur('Auteur de la première candidature');
+        $application1->setContenu('Contenu de la première candidature');
+
+        //création de l'entité annonce 2
+        $application2 = new Application();
+        $application2->setAuteur('Auteur de la deuxième candidature');
+        $application2->setContenu('Contenu de la deuxième candidature');
+
+        //création de l'entité annonce 2
+        $application3 = new Application();
+        $application3->setAuteur('Auteur de la troisième candidature');
+        $application3->setContenu('Contenu de la troisième candidature');
+
+        //on lie les candidatures à l'annonce
+        $application1->setAnnonce($annonce);
+        $application2->setAnnonce($annonce);
+        $application3->setAnnonce($annonce);
+
         //création de l'entité image
         $image = new Image();
         $image->setUrl('http://sdz-upload.s3.amazonaws.com/prod/upload/job-de-reve.jpg');
@@ -84,8 +110,13 @@ class AnnonceController extends Controller
         //on lie l'image à l'annonce
         $annonce->setImage($image);
 
+        //récupération de l'entity manager + persistance et envoie en bdd (flush)
         $em = $this->getDoctrine()->getManager();
         $em->persist($annonce);
+        //persistance des candidatures puisque la relation n'est pas en cascade
+        $em->persist($application1);
+        $em->persist($application2);
+        $em->persist($application3);
         $em->flush();
 
 
---
+++ src/OC/PlatformBundle/Entity/Application.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace OC\PlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Application
+ *
+ * @ORM\Table(name="application")
+ * @ORM\Entity(repositoryClass="OC\PlatformBundle\Repository\ApplicationRepository")
+ */
+class Application
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="auteur", type="string", length=255)
+     */
+    private $auteur;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contenu", type="string", length=255)
+     */
+    private $contenu;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+
+    /**
+     * @ORM\ManyToOne(targetEntity="OC\PlatformBundle\Entity\Annonce", cascade={"persist"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $annonce;
+
+    /**
+     * @return mixed
+     */
+    public function getAnnonce()
+    {
+        return $this->annonce;
+    }
+
+    /**
+     * @param mixed $annonce
+     */
+    public function setAnnonce(Annonce $annonce)
+    {
+        $this->annonce = $annonce;
+
+        //todo : peut être rajouter un return $this;
+    }
+
+    /**
+     * Application constructor.
+     *
+     * @param \DateTime $date
+     */
+    public function __construct()
+    {
+        $this->date = new \DateTime();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set auteur
+     *
+     * @param string $auteur
+     *
+     * @return Application
+     */
+    public function setAuteur($auteur)
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    /**
+     * Get auteur
+     *
+     * @return string
+     */
+    public function getAuteur()
+    {
+        return $this->auteur;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param string $contenu
+     *
+     * @return Application
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    /**
+     * Get contenu
+     *
+     * @return string
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Application
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
+
